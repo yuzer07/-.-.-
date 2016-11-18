f3e24a96f3e845d0c156fe4549dbfdf7a9d933a2
Kiryl <yuzer1993@gmail.com>
2016-11-18 18:58:50 +0300

    Arguments
    2. Отобразить в окне консоли аргументы командной строки в обратном порядке.

---
+++ Arguments.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Кирилл on 17.10.2016.
+ */
+public class Arguments {
+
+    public static void main(String [] args){
+        for (int i = args.length - 1; i >= 0; i-- );
+    }
+}
