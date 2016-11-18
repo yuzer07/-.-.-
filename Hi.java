a021404022d980c2586d669dfc1ba1ed9729e0bb
Kiryl <yuzer1993@gmail.com>
2016-11-18 19:08:20 +0300

    Hi
    Приветствовать любого пользователя при вводе его имени через командную строку.

---
+++ Hi.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Created by Кирилл on 17.11.2016.
+ */
+public class Hi {
+    public static void main (String[]args){
+        Scanner a = new Scanner(System.in);
+        System.out.println("Введите имя:");
+        String name = a.nextLine();
+        System.out.println("Привет " + name);
+        a.close();
+        }
+    }
